4ce8ae1508dc9e1c5ee3f7cf1d0ce7092530505a
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-07-11 14:12:37 +0500

    Load Images and fonts in Webpack Config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash'
 import './style.css'
+import Headshot from'./Headshot.jpg'
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('img');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.src = Headshot
 
   return element;
 }
